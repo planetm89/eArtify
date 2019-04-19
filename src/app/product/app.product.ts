d3070cf9ac77ffedbb5d620c5222833908015f90
Shrikrishna Prabhumirashi <32734116+svpmirashi@users.noreply.github.com>
2019-04-20 00:44:21 +0530

    Product class and related changes
    Product class and related changes

---
+++ src/app/product/app.product.ts
@@ -0,0 +1,15 @@
+export class Product{
+    public type_ID : Number
+    name : String
+    material_ID : Number
+    zipCloser : boolean
+    price : Number
+    color: String
+    height : Number
+    width: Number
+    breadth: Number
+    description: String
+    audience_ID : Number
+    thumbnail_Image_Id : Number
+    image_url: String
+}
--- src/app/product/app.productcomponent.ts
+++ src/app/product/app.productcomponent.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './app.product';
 
-export interface Tile {
-    cols: number;
-    rows: number;
-    text: string;
-    border: string;
-   }
 
 @Component({
     selector:'app-product',
@@ -14,15 +9,25 @@ export interface Tile {
 
 export class ProductComponent{
 
-    tiles: Tile[] = [
-        {text: 'Tile 1', cols: 1, rows: 1 ,border: '3px double purple'},
-        {text: 'Tile 2', cols: 1, rows: 1 ,border: '3px double red'},
-        {text: 'Tile 3', cols: 1, rows: 1 ,border: '3px double skyblue'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
-        {text: 'Tile 4', cols: 1, rows: 1 ,border: '3px double yellow'},
+    public products: Product[] = [
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 1, zipCloser:true, audience_ID:1, breadth:5, color:'red', description:'red bag', height:10, material_ID:1, name : 'Red jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/1.jpg'},
+        {type_ID: 2, zipCloser:true, audience_ID:1, breadth:5, color:'blue', description:'blue bag', height:10, material_ID:1, name : 'Blue jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/2.jpg'},
+        {type_ID: 3, zipCloser:true, audience_ID:1, breadth:5, color:'green', description:'green bag', height:10, material_ID:1, name : 'Green jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/3.jpg'},
+        {type_ID: 4, zipCloser:true, audience_ID:1, breadth:5, color:'yellow', description:'yellow bag', height:10, material_ID:1, name : 'Yellow jute bag', price : 150, thumbnail_Image_Id: 1 ,width : 10, image_url:'./assets/images/4.jpg'}
         ];
 }
